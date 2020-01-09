670669eed7c956153ecde334c2aa24381045c58d
abutnal <45108373+abutnal@users.noreply.github.com>
2020-01-09 11:33:21 +0530

    Update quick_sort.js

--- quick_sort.js
+++ quick_sort.js
@@ -1,27 +1,23 @@
-function quick_Sort(origArray) {
-	if (origArray.length <= 1) { 
-		return origArray;
-		
-	} else {
+const quickSort = (arr) =>{
+    if(arr.length<2){
+        return arr;
+    }else{
+        var pivot = arr[arr.length - 1];
         var left = [];
-		var right = [];
-		var newArray = [];
-		var pivot = origArray.pop();
-		
-		var length = origArray.length;
-
-		for (var i = 0; i < length; i++) {
-			if (origArray[i] <= pivot) {
-				left.push(origArray[i]);
-			} else {
-				right.push(origArray[i]);
-			}
-		}
-		return newArray.concat(quick_Sort(left), pivot, quick_Sort(right));
-	}
+        var right = [];
+        
+        for(var i = 0; i<arr.length - 1; i++){
+            if(arr[i]<pivot){
+                left.push(arr[i]);
+            }else{
+                right.push(arr[i]);
+            }
+        }
+        return [...left, pivot, ...right];
+    }
+    
 }
 
-var myArray = [3, 0, 2, 5, -1, 4, 1 ];
-console.log("Original array: " + myArray);
-var sortedArray = quick_Sort(myArray);
-console.log("Sorted array: " + sortedArray);
+const arr = [10,12,5,20,9,4,-2,0];
+const output = quickSort(arr);
+console.log(output);
